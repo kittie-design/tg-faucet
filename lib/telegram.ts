6a043f5c27e654e58310125febe4d0a44bf6eb1d
kittie-design <marysheilawhite@gmail.com>
2026-07-03 12:33:48 +0200

    Delete telegram.ts

--- lib/telegram.ts
+++
@@ -1,8 +0,0 @@
-
-  export function getTelegramUser() {
-  if (typeof window === "undefined") return null;
-
-  const tg = (window as any)?.Telegram?.WebApp;
-
-  return tg?.initDataUnsafe?.user || null;
-}
